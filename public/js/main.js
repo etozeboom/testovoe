ad96238fe1655de050bac77e792c2ac5d90a806b
Andrey <andrey_shuchkin@mail.ru>
2019-01-27 17:06:32 +0300

    for modals js i

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,11 @@
 class Popup {
     constructor(options) {
         this.modal = document.querySelector(options.modal);
-        this.form = document.querySelector(options.form);
         this.overlay = document.querySelector(options.overlay);
-        this.door_id = document.querySelector(options.door_id);
         var popup = this;
         this.open = function (content) {
             //popup.modal.append = content;
-            popup.door_id.innerHTML = content;
+            //popup.door_id.innerHTML = content;
             popup.overlay.classList.add('open');
             popup.modal.classList.add('open');
         };
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -7,75 +7,84 @@
             <div class="card">
                 <div class="card-header">Doors</div>
                 <div class="doors">
+                    <script>/*
+                        class Popup {
+                        constructor(options) {
+                            this.modal = document.querySelector(options.modal);
+                            this.overlay = document.querySelector(options.overlay);
+                            var popup = this;
+                            this.open = function () {
+                                //popup.modal.append = content;
+                                //popup.door_id.innerHTML = content;
+                                popup.overlay.classList.add('open');
+                                popup.modal.classList.add('open');
+                            };
+                            this.close = function () {
+                                popup.overlay.classList.remove('open');
+                                popup.modal.classList.remove('open');
+                                popup.modal.classList.remove('open');
+                            };
+                            this.overlay.onclick = popup.close;
+                        }
+                    }*/
+                    </script>
                     @foreach($doors as $door)
-                    <a id="{{ $door->id}}"  class="door{{ $door->id}} btn">{{ $door->name}}</a>
+                        <a id="{{ $door->id}}"  class="door{{ $door->id}} btn">{{ $door->name}}</a>
+
+                        <div class="modal{{ $door->id}} modal">
+                            <form method="POST" action="{{ route('index') }}">
+                                @csrf
+
+                                <div class="form-group row justify-content-center">
+                                    <label for="name" class="col-md-12 col-form-label text-md-center">{{ __('Username') }}</label>
+
+                                    <div class="col-md-8 align-self-center">
+                                        <input  disabled="disabled" id="name{{ $door->id}}" type="name" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="name" value="{{  $request->user()->first()->name }}" required autofocus>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row justify-content-center">
+                                    <label for="password" class="col-md-12 col-form-label text-md-center">{{ __('Password') }}</label>
+
+                                    <div class="col-md-8">
+                                        <input id="password{{ $door->id}}" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-8 offset-md-8">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Check') }}
+                                        </button>
+
+                                        <input type="hidden" name="door_id" value=' {{$door->id}}'>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                        <script>window.onload = function(){
+                            var p{{$door->id}} = new Popup({
+                                modal: '.modal{{$door->id}}',
+                                overlay: '.overlay',
+                            });
+                            document.querySelector('.doors .door{{$door->id}}').onclick = function(){
+                                p{{$door->id}}.open();
+                            };}
+                        </script>
                     @endforeach
                 </div>
-                <div class="modal">
-                    <form method="POST" action="{{ route('index') }}">
-                            @csrf
-
-                            <div class="form-group row justify-content-center">
-                                <label for="name" class="col-md-12 col-form-label text-md-center">{{ __('Username') }}</label>
-
-                                <div class="col-md-8 align-self-center">
-                                    <input  disabled="disabled" id="name" type="name" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="name" value="{{  $request->user()->first()->name }}" required autofocus>
-                                </div>
-                            </div>
-
-                            <div class="form-group row justify-content-center">
-                                <label for="password" class="col-md-12 col-form-label text-md-center">{{ __('Password') }}</label>
-
-                                <div class="col-md-8">
-                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                    @if ($errors->has('password'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-8">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Check') }}
-                                    </button>
-
-                                    <div class="door_id"></div>
-                                </div>
-                            </div>
-
-                        </form>
-                </div>
+               
                 <div class="overlay"></div>
             </div>
         </div>
     </div>
 </div>
-<script>
-    window.onload = function(){
-        var p = new Popup({
-            modal: '.modal',
-            overlay: '.overlay',
-            form: '.modal form',
-            door_id: '.modal form .door_id',
-        });
 
-        var inputs = document.querySelectorAll('.doors .btn');
-    
-        //alert(inputs.length);
-        for(var i = 1; i < inputs.length+1; i++){
-            //alert(i);
-            document.querySelector('.doors .door'+i).onclick = function(){
-             p.open('<input type="hidden" name="door_id" value="'+i+'">');
-            };
-        }
-                
-            
-       
-
-    }
-</script>
 @endsection
