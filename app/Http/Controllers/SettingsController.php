6c53c5c6df41ebc0e012d1e5a7955d456607b724
aschuchkin <aschuchkin@zuzex.com>
2019-01-28 15:05:05 +0300

    done settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class SettingsController extends Controller
 {
@@ -12,25 +13,41 @@ class SettingsController extends Controller
 		
 		
     }
-    
+    public $messages;
     public function execute(Request $request) 
     {
+        
         if(!Auth::check()) {
 			abort(403);
-		}
+        }
+        //dump($value = $request->session()->get('success'));
+        
         if($request->isMethod('post')) 
         {
             $data = $request->all();
             if (Hash::check($data['password'], Auth::user()->password)) 
             {
+                //dump(request()->user()->name);
+                //dump($data['name']);
+                if($data['name']!=$roles = request()->user()->name )
+                {
+                    $this->changeName();
+                }
+                if($data['email']!=$roles = request()->user()->email )
+                {
+                    $this->changeEmail();
+                }
+               
+                if($data['newpassword'])
+                {
+                    $this->changePassword();
+                }
 
-
-
-
-
+                //dump($this->messages);
+                return view('site.settings',['request'=> $request, 'messages' => $this->messages]);
             }
-            
-            return view('site.settings',['status' => 'неверный пароль']);
+
+            return view('site.settings',['request'=> $request,'status' => 'неверный пароль']);
         }
 
 
@@ -42,14 +59,36 @@ class SettingsController extends Controller
     public function changePassword()
     {
         $this->validate(request(), [
-            'new_password' => 'required|string|min:6|confirmed',
+            'newpassword' => 'required|string|min:6|confirmed',
         ]);
 
         request()->user()->fill([
-            'password' => Hash::make(request()->input('new_password'))
+            'password' => Hash::make(request()->input('newpassword'))
         ])->save();
-        request()->session()->flash('success', 'Password changed!');
-
-        return redirect()->route('password.change');
+        //request()->session()->flash('success', 'Password changed!');
+        $this->messages [] = 'Password changed | ';
+        
+    }
+    public function changeName()
+    { 
+        $this->validate(request(), [
+            'name' => 'required|string|min:1',
+        ]);
+       
+        request()->user()->fill([
+            'name' => request()->input('name')
+        ])->save();
+        $this->messages [] = 'Name changed | ';
+    }
+    public function changeEmail()
+    { 
+        $this->validate(request(), [
+            'email' => 'required|email|unique:users|string|max:255',
+        ]);
+       
+        request()->user()->fill([
+            'email' => request()->input('email')
+        ])->save();
+        $this->messages [] = 'email changed | ';
     }
 }
--- resources/views/site/settings.blade.php
+++ resources/views/site/settings.blade.php
@@ -9,12 +9,25 @@
                 
                     <form method="POST" action="{{ route('settings') }}">
                         @csrf
-
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
                         <div class="form-group row justify-content-center">
                             <label for="name" class="col-md-12 col-form-label text-md-center">{{ __('Username') }}</label>
 
                             <div class="col-md-8 align-self-center">
-                                <input id="name" type="name" class="form-control" name="name" @if (Auth::check()) value="{{  Auth::user()->name }}" @endif required autofocus>
+                                <input id="name" type="name" class="form-control" name="name" @if (Auth::check()) value="@if(null != old('name')){{old('name')}}@else{{Auth::user()->name}}@endif" @endif required autofocus>
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
                         
@@ -22,7 +35,12 @@
                             <label for="email" class="col-md-12 col-form-label text-md-center">{{ __('Email') }}</label>
 
                             <div class="col-md-8 align-self-center">
-                                <input id="email" type="email" class="form-control" name="email" @if (Auth::check()) value="{{  Auth::user()->email }}" @endif required autofocus>
+                                <input id="email" type="email" class="form-control" name="email" @if (Auth::check()) value="@if(null != old('email')){{old('email')}}@else{{Auth::user()->email}}@endif" @endif required autofocus>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
@@ -32,9 +50,9 @@
                             <div class="col-md-8">
                                 <input id="newpassword" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="newpassword" >
 
-                                @if ($errors->has('password'))
+                                @if ($errors->has('new_password'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                        <strong>{{ $errors->first('new_password') }}</strong>
                                     </span>
                                 @endif
                             </div>
@@ -66,8 +84,15 @@
                             </div>
                         </div>
                         @if (isset($status) )
-                            <div class="alert alert-success" role="alert">
+                            <div class="alert alert-danger" role="alert">
                                 {{ $status }}
+                            </div>
+                        @endif
+                        @if (isset($messages) )
+                            <div class="alert alert-success" role="alert">
+                            @foreach($messages as $message)
+                                 {{ $message }}
+                            @endforeach
                             </div>
                         @endif
                         
